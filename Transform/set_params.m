f8360916870442f7269c8e76f9ca9f563b24693d
David Schmid <schmid.david.ds@gmail.com>
2021-12-22 16:25:17 +0000

    changed from // to % for commenting out a line of code

--- Transform/set_params.m
+++ Transform/set_params.m
@@ -111,7 +111,7 @@ if(ischar(params.message))
     %read message from file
     
     cd ..;
-//urlname = ['file:///' fullfile(pwd,params.message)];    
+% urlname = ['file:///' fullfile(pwd,params.message)];    
     urlname = ['file://' strrep(fullfile(pwd,params.message), " ", "%20")]
 cd Transform;
     try
